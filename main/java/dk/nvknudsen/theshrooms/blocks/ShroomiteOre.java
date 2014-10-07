22e1fec8d5c9a71fcb275b17b8e9ec99b9b2c230
Nicklas Vedsted <nicklasvedsted@hotmail.com>
2014-10-07 13:09:35 +0200

    Moved blocks and items.

---
+++ main/java/dk/nvknudsen/theshrooms/blocks/ShroomiteOre.java
@@ -0,0 +1,27 @@
+package dk.nvknudsen.theshrooms.blocks;
+
+import java.util.Random;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import sun.net.www.content.text.Generic;
+
+public class ShroomiteOre extends Block
+{
+
+	public ShroomiteOre(int par1)
+	{
+		
+		// All the properties
+		super(par1, Material.rock);
+		this.setCreativeTab(CreativeTabs.tabBlock);
+		this.setHardness(2.0F);
+		this.setStepSound(soundStoneFootstep);
+		this.setUnlocalizedName("ShroomiteOre");
+		this.setLightValue(0.5F);
+		this.setTextureName("theshrooms:ShroomiteOre");
+		
+	}
+}
---
+++ main/java/dk/nvknudsen/theshrooms/items/ShroomiteIngot.java
@@ -0,0 +1,20 @@
+package dk.nvknudsen.theshrooms.items;
+
+import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+
+public class ShroomiteIngot extends Item
+{
+
+	public ShroomiteIngot(int par1)
+	{
+		
+		super(par1);
+		this.setCreativeTab(CreativeTabs.tabMaterials);
+		this.setUnlocalizedName("ShroomiteIngot");
+		this.setMaxStackSize(64);
+		this.setTextureName("theshrooms:ShroomiteIngot");
+		
+	}
+}
