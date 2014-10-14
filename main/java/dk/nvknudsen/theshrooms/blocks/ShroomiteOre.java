af70af4b3626ce76760af11ba135f2ccf09736b0
Nicklas Vedsted <nicklasvedsted@hotmail.com>
2014-10-14 18:28:17 +0200

    FIX: Removed unintentional feature
     - Removed the Shroombie spawning, when breaking Shroomite ore.

--- main/java/dk/nvknudsen/theshrooms/blocks/ShroomiteOre.java
+++ main/java/dk/nvknudsen/theshrooms/blocks/ShroomiteOre.java
@@ -3,8 +3,6 @@ package dk.nvknudsen.theshrooms.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
-import net.minecraft.world.World;
-import dk.nvknudsen.theshrooms.mob.Shroombie;
 
 public class ShroomiteOre extends Block
 {
@@ -20,15 +18,5 @@ public class ShroomiteOre extends Block
 		this.setUnlocalizedName("ShroomiteOre");
 		this.setTextureName("theshrooms:ShroomiteOre");
 		
-	}
-	
-	public void onBlockDestroyedByPlayer(World w, int x, int y, int z, int meta)
-	{
-		if(!w.isRemote)
-		{
-			Shroombie s=new Shroombie(w);
-			s.setPosition(x, y, z);
-			w.spawnEntityInWorld(s);
-		}
 	}
 }
